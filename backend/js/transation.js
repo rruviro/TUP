5e79dd9f4ea381a2176befde3b3147b67efaaf13
rruviro <138757113+rruviro@users.noreply.github.com>
2024-11-02 15:27:46 +0800

    UNKOWN

---
+++ backend/js/transation.js
@@ -0,0 +1,12 @@
+function navigateWithFade(url) {
+    document.body.classList.add('fade-out'); // Trigger fade-out effect
+    setTimeout(() => {
+      window.location.href = url; // Navigate to the new page
+    }, 0); // Match the delay with the animation duration (1 second here)
+  }
+  
+  // Trigger fade-in on page load
+  window.addEventListener('load', () => {
+    document.body.classList.add('fade-in');
+  });
+  
